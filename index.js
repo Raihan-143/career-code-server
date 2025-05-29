497603d06982e09805d7ffc50057d561b3f5be1f
Raihan-143 <raihan172hasan@gmail.com>
2025-05-30 05:33:31 +0600

    using patch method for getting viewApplicant details with their status

--- index.js
+++ index.js
@@ -96,6 +96,19 @@ async function run() {
             res.send(result);
         });
 
+        //using patch method for getting viewApplicant details
+        app.patch('/applications/:id', async(req,res)=>{
+            const id=req.params.id;
+            const filter={_id: new ObjectId(id)}
+            const updatedDoc={
+                $set:{
+                    status: req.body.status
+                }
+            }
+            const result = await applicationsCollection.updateOne(filter,updatedDoc)
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
